d318c0c2406084642a05b73753b89a67c0ade63c
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-22 17:21:35 +0100

    CIT-11 creer arbre

---
+++ src/main/java/com/wora/citronix/controllers/ArbreControlleur.java
@@ -0,0 +1,57 @@
+package com.wora.citronix.controllers;
+
+import com.wora.citronix.dtos.arbre.ArbreCreateDTO;
+import com.wora.citronix.dtos.arbre.ArbreDTO;
+import com.wora.citronix.dtos.arbre.ArbreUpdateDTO;
+import com.wora.citronix.services.ServiceInerf.ArbreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/arbres")
+@RequiredArgsConstructor
+public class ArbreControlleur {
+    private final ArbreService arbreService;
+
+    @PostMapping
+    public ResponseEntity<ArbreDTO> createArbre(@RequestBody @Valid ArbreCreateDTO arbreCreateDTO) {
+        ArbreDTO savedArbre = arbreService.save(arbreCreateDTO);
+        return ResponseEntity.status(201).body(savedArbre);
+    }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<ArbreDTO> updateArbre(
+//            @RequestBody @Valid ArbreUpdateDTO arbreUpdateDTO, @RequestParam Long id) {
+//
+//
+//        ArbreDTO updatedArbre = arbreService.update(arbreUpdateDTO, id);
+//
+//        if (updatedArbre == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(updatedArbre);
+//    }
+//
+//
+//    @GetMapping
+//    public ResponseEntity<List<ArbreDTO>> getAllArbres(@RequestParam int page, @RequestParam int size) {
+//        List<ArbreDTO> ferms = arbreService.findAll(page, size);
+//        return ResponseEntity.ok(ferms);
+//    }
+//
+//    @GetMapping("/by_champ")
+//    public ResponseEntity<List<ArbreDTO>> getArbresByChampId(@RequestParam Long champId, @RequestParam int page, @RequestParam int size) {
+//        List<ArbreDTO> ferms = arbreService.findByChampId(champId,page, size);
+//        return ResponseEntity.ok(ferms);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable Long id) {
+//        arbreService.delete(id);
+//        return ResponseEntity.noContent().build();
+//    }
+}
---
+++ src/main/java/com/wora/citronix/services/ServiceImpl/ArbreServiceImpl.java
@@ -0,0 +1,81 @@
+package com.wora.citronix.services.ServiceImpl;
+
+import com.wora.citronix.Mappers.ArbreMapper;
+import com.wora.citronix.dtos.arbre.ArbreCreateDTO;
+import com.wora.citronix.dtos.arbre.ArbreDTO;
+import com.wora.citronix.dtos.arbre.ArbreUpdateDTO;
+import com.wora.citronix.entities.Arbre;
+import com.wora.citronix.entities.Champ;
+import com.wora.citronix.repositories.ArbreRepository;
+import com.wora.citronix.repositories.ChampRepository;
+import com.wora.citronix.services.ServiceInerf.ArbreService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ArbreServiceImpl implements ArbreService {
+    private final ChampRepository champRepository;
+    private final ArbreMapper arbreMapper;
+    private final ArbreRepository arbreRepo;
+
+    public ArbreDTO save(ArbreCreateDTO createDto) {
+        Optional<Champ> champOptional = champRepository.findById(createDto.getChampId());
+        if (champOptional.isEmpty()) {
+            throw new EntityNotFoundException("Champ non trouvé");
+        }
+        Champ champ = champOptional.get();
+        createDto.setAge(createDto.getDatePlantation());
+        Arbre arbre = arbreMapper.toEntity(createDto);
+        arbre.setChamp(champ);
+
+        Arbre savedArbre = arbreRepo.save(arbre);
+        return arbreMapper.toDTO(savedArbre);
+
+    }
+
+    public ArbreDTO findById(Long id){
+        Arbre arbre = arbreRepo.findById(id).orElseThrow(EntityNotFoundException::new);
+        return arbreMapper.toDTO(arbre);
+    }
+
+    public ArbreDTO update(ArbreUpdateDTO updateDto, Long id) {
+        Arbre arbre = arbreRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("arbre not found"));
+        Champ champ = champRepository.findById(updateDto.getChampId()).orElseThrow(() -> new EntityNotFoundException("champ not found"));
+        if (arbre != null) {
+            arbre.setDatePlantation(updateDto.getDatePlantation());
+            arbre.setAge(updateDto.getAge());
+            arbre.setChamp(champ);
+//            arbre.setProductif(updateDto.get);
+            arbre = arbreRepo.save(arbre);
+        }
+        return arbreMapper.toDTO(arbre);
+    }
+
+    public List<ArbreDTO> findAll(int page, int size) {
+        PageRequest pageable = PageRequest.of(page, size);
+        return arbreRepo.findAll(pageable).stream()
+                .map(arbreMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ArbreDTO> findByChampId(Long champId, int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return arbreRepo.findByChampId(champId,pageable).stream()
+                .map(arbreMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(Long id){
+        arbreRepo.deleteById(id);
+    }
+}
